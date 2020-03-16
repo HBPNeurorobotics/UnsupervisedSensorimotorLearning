0984738e7b70862c4ac539aafdf6fc49fc1b1f97
Michael Zechmair <michael.zechmair@in.tum.de>
2020-03-16 11:19:53 +0000

    Added synchronous IBA data exchange Added brain to IBA leg file

--- files/StorageExperiments/IBA/arm/FreqExtr_Module.py
+++ files/StorageExperiments/IBA/arm/FreqExtr_Module.py
@@ -6,6 +6,7 @@ from gazebo_msgs.msg import LinkStates
 from sensor_msgs.msg import JointState
 from gazebo_msgs.srv import ApplyBodyWrench
 from geometry_msgs.msg import Wrench, Vector3
+from threading import Lock
 import math
 import numpy as np
 from numpy.linalg import norm
@@ -22,96 +23,96 @@ class FreqExtr_Module(ExternalModule):
         super(FreqExtr_Module, self).__init__(module_name, steps)
 
     def jointStateCallback(self, data):
-        self.joint_states = data
-    def weightsCallback(self, data):
-        self.weights = data
+        with self.joint_state_lock:
+            self.joint_states = data
 
     def initialize(self):
         rospy.wait_for_service("gazebo/apply_joint_effort")
         rospy.wait_for_service("gazebo/clear_joint_forces")
-	self.applyEffortService = None
-        self.clearJointService  = None
+        self.applyEffortService = rospy.ServiceProxy("gazebo/apply_joint_effort", gazebo_msgs.srv.ApplyJointEffort)
+        self.clearJointService = rospy.ServiceProxy("gazebo/clear_joint_effort", gazebo_msgs.srv.JointRequest)
 
         self.theta1 = 0.0
         self.theta2 = 0.0
 
+        self.joint_state_lock = Lock()
         self.joint_states = None
         self.joint_state_sub = rospy.Subscriber("/joint_states", JointState, self.jointStateCallback, queue_size=1)
+        
         self.weights = [0.0, 0.0, 0.0]
-        self.weights_sub = rospy.Subscriber("/weights", Vector3, self.weightsCallback, queue_size=1)
 
     def run_step(self):
         weight_change = 3
 
-        if self.joint_states is not None:
+        if len(self.synced_data.m1) is 4:
+            self.weights = self.synced_data.m1[1:]
 
-            if self.applyEffortService.value is None:
-                self.applyEffortService.value = rospy.ServiceProxy("/gazebo/apply_joint_effort", ApplyJointEffort)
-            if self.clearJointService.value is None:
-                self.clearJointService.value = rospy.ServiceProxy("/gazebo/clear_joint_forces", JointRequest)
+        with self.joint_state_lock:
+            if self.joint_states is not None:
+                def clearJoints():
+                    self.clearJointService.value('HumerusBone_shoulder')
+                    self.clearJointService.value('RadiusBone_ellbow')
 
-            def clearJoints():
-                self.clearJointService.value('HumerusBone_shoulder')
-                self.clearJointService.value('RadiusBone_ellbow')
+                #################### Definitions ###################
+                ## read out current positions of the joints from /joint_states
+                phi1 = self.joint_states.value.position[1]
+                phi2 = self.joint_states.value.position[2]
+                dq1  = self.joint_states.value.velocity[1]
+                dq2  = self.joint_states.value.velocity[2]
+                q1 = phi1
+                q2 = phi1+phi2
+                
+                #################### Parameters ###################
+                ## set threshold, spring stiffness and damping (k and d should correspond to values in SDF file)
+                threshold = 1.0 # Nm
+                k         = 10  # Nm/rad
+                d         = 0.7 # Nm/rad
+                w_default = [-1, 1]
+                
+                if self.weights.value is None:
+                    w = w_default
+                else:
+                    w = [self.weights.value.x, self.weights.value.y]
 
-            #################### Definitions ###################
-            ## read out current positions of the joints from /joint_states
-            phi1 = self.joint_states.value.position[1]
-            phi2 = self.joint_states.value.position[2]
-            dq1  = self.joint_states.value.velocity[1]
-            dq2  = self.joint_states.value.velocity[2]
-            q1 = phi1
-            q2 = phi1+phi2
+                #################### Controller ###################
+                ## set weights
+                weights_tau = np.array([w[0], w[1]])
+                weights_theta = np.array([w[0], w[1]])
+                
+                ## get joint torques at beginning of step
+                tau_tot_1 = k*(self.theta1.value-q1)
+                tau_tot_2 = k*(self.theta2.value-q2)
+                tau_tot = np.array([tau_tot_1, tau_tot_2])
+                
+                ## reduce dimension: set threshold and choose control value theta_z according to threshold
+                if norm(w)==0:
+                    tau_z = 0.0
+                    weights_theta = w_default
+                else:
+                    tau_z = weights_tau.dot(tau_tot)/norm(weights_tau)
 
-            #################### Parameters ###################
-            ## set threshold, spring stiffness and damping (k and d should correspond to values in SDF file)
-            threshold = 1.0 # Nm
-            k         = 10  # Nm/rad
-            d         = 0.7 # Nm/rad
-            w_default = [-1, 1]
+                if tau_z > threshold:
+                    theta_z = 0.3
+                elif -threshold <= tau_z and tau_z <= threshold:
+                    theta_z = 0.0
+                elif tau_z < -threshold:
+                    theta_z = -0.3
 
-            if self.weights.value is None:
-                w = w_default
-            else:
-                w = [self.weights.value.x, self.weights.value.y]
+                ## go back to 2 dimension
+                self.theta1.value = weights_theta[0]*theta_z/norm(weights_theta)
+                self.theta2.value = weights_theta[1]*theta_z/norm(weights_theta)
 
-            #################### Controller ###################
-            ## set weights
-            weights_tau = np.array([w[0], w[1]])
-            weights_theta = np.array([w[0], w[1]])
+                ## calculate motor signal
+                tau1 = (self.theta1.value)*(k)
+                tau2 = (self.theta2.value)*(k)
 
-            ## get joint torques at beginning of step
-            tau_tot_1 = k*(self.theta1.value-q1)
-            tau_tot_2 = k*(self.theta2.value-q2)
-            tau_tot = np.array([tau_tot_1, tau_tot_2])
+                clearJoints()
+                if t>1: ## otherwise gazebo crashes
+                    self.applyEffortService.value('myoarm::HumerusBone_shoulder', tau1, rospy.Time(), rospy.Duration(-1))
+                    self.applyEffortService.value('myoarm::RadiusBone_ellbow', tau2, rospy.Time(), rospy.Duration(-1))
+                    #if duration < 0 apply continuously
 
-            ## reduce dimension: set threshold and choose control value theta_z according to threshold
-            if norm(w)==0:
-                tau_z = 0.0
-                weights_theta = w_default
-            else:
-                tau_z = weights_tau.dot(tau_tot)/norm(weights_tau)
-
-            if tau_z > threshold:
-                theta_z = 0.3
-            elif -threshold <= tau_z and tau_z <= threshold:
-                theta_z = 0.0
-            elif tau_z < -threshold:
-                theta_z = -0.3
-
-            ## go back to 2 dimension
-            self.theta1.value = weights_theta[0]*theta_z/norm(weights_theta)
-            self.theta2.value = weights_theta[1]*theta_z/norm(weights_theta)
-
-            ## calculate motor signal
-            tau1 = (self.theta1.value)*(k)
-            tau2 = (self.theta2.value)*(k)
-
-            clearJoints()
-            if t>1: ## otherwise gazebo crashes
-                self.applyEffortService.value('myoarm::HumerusBone_shoulder', tau1, rospy.Time(), rospy.Duration(-1))
-                self.applyEffortService.value('myoarm::RadiusBone_ellbow', tau2, rospy.Time(), rospy.Duration(-1))
-                #if duration < 0 apply continuously
+                self.module_data = [1, tau1, tau2]
 
     #def shutdown(self):
     #    pass
--- files/StorageExperiments/IBA/arm/setWeight_Module.py
+++ files/StorageExperiments/IBA/arm/setWeight_Module.py
@@ -4,6 +4,7 @@ from std_msgs.msg import Float64
 from std_msgs.msg import Int64
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import Vector3
+from threading import Lock
 import math
 import numpy as np
 from numpy.linalg import norm
@@ -20,7 +21,9 @@ class setWeights_Module(ExternalModule):
         super(setWeights_Module, self).__init__(module_name, steps)
 
     def jointStateCallback(self, data):
-        self.joint_states = data
+        with self.joint_state_lock:
+            self.joint_states = data
+
     def setWeightCallback(self, data):
         self.set_weights = data
 
@@ -30,6 +33,7 @@ class setWeights_Module(ExternalModule):
         self.weight1_gd  =math.sqrt(2)
         self.weight2_gd =-math.sqrt(2)
 
+        self.joint_state_lock = Lock()
         self.joint_states = None
         self.joint_state_sub = rospy.Subscriber("/joint_states", JointState, self.jointStateCallback, queue_size=1)
         self.set_weights = None
@@ -38,65 +42,66 @@ class setWeights_Module(ExternalModule):
         self.weight_pub = rospy.Publisher("/weights", Vector3, queue_size=1)
 
     def run_step(self):
-        if self.joint_states is not None:
+        with self.joint_state_lock:
+            if self.joint_states is not None:
 
-            if self.set_weight.value == None:
-                weight_change = 2
-            else:
-                weight_change = self.set_weight.value.data
+                if self.set_weight.value == None:
+                    weight_change = 2
+                else:
+                    weight_change = self.set_weight.value.data
 
-            if np.isnan(self.weight1_gd.value) == True:
-                self.weight1_gd.value = math.sqrt(2)
-                self.weight2_gd.value = -math.sqrt(2)
+                if np.isnan(self.weight1_gd.value) == True:
+                    self.weight1_gd.value = math.sqrt(2)
+                    self.weight2_gd.value = -math.sqrt(2)
 
-            ## read out current positions of the joints from /joint_states
-            phi1 = self.joint_states.value.position[1]
-            phi2 = self.joint_states.value.position[2]
-            q1 = phi1
-            q2 = phi1+phi2
+                ## read out current positions of the joints from /joint_states
+                phi1 = self.joint_states.value.position[1]
+                phi2 = self.joint_states.value.position[2]
+                q1 = phi1
+                q2 = phi1+phi2
 
-            #################### Parameters ###################
-            ## set time from which recording of data (for CSV) should start
+                #################### Parameters ###################
+                ## set time from which recording of data (for CSV) should start
 
-            if weight_change == 0:
-                w = [0, 0]
-            elif weight_change == 1:
-                w = [-1, 1]
-            elif weight_change == 2:
-                w =[self.weight1_oja.value, self.weight2_oja.value]
-            elif weight_change == 3:
-                w =[self.weight1_gd.value,  self.weight2_gd.value]
+                if weight_change == 0:
+                    w = [0, 0]
+                elif weight_change == 1:
+                    w = [-1, 1]
+                elif weight_change == 2:
+                    w =[self.weight1_oja.value, self.weight2_oja.value]
+                elif weight_change == 3:
+                    w =[self.weight1_gd.value,  self.weight2_gd.value]
 
-            weights = Vector3(w[0], w[1], 0.0)
-            self.weight_pub.send_message(weights)
+                weights = Vector3(w[0], w[1], 0.0)
+                self.weight_pub.send_message(weights)
+                self.module_data = [0, w[0], w[1], 0.0]
 
 
-            #################### Oja rule ###################
-            ## set parameters
-            dt = 0.02 #sec --> default NRP update rate (CLE)
-            gamma = 0.5 #Hz
+                #################### Oja rule ###################
+                ## set parameters
+                dt = 0.02 #sec --> default NRP update rate (CLE)
+                gamma = 0.5 #Hz
 
-            ## calculate rate change
-            dw1_oja = gamma*(self.weight1_oja.value*q1+self.weight2_oja.value*q2)*(q1-((self.weight1_oja.value*q1+self.weight2_oja.value*q2)*self.weight1_oja.value))
-            dw2_oja = gamma*(self.weight1_oja.value*q1+self.weight2_oja.value*q2)*(q2-((self.weight1_oja.value*q1+self.weight2_oja.value*q2)*self.weight2_oja.value))
-            self.weight1_oja.value = self.weight1_oja.value+(dw1_oja*dt)
-            self.weight2_oja.value = self.weight2_oja.value+(dw2_oja*dt)
+                ## calculate rate change
+                dw1_oja = gamma*(self.weight1_oja.value*q1+self.weight2_oja.value*q2)*(q1-((self.weight1_oja.value*q1+self.weight2_oja.value*q2)*self.weight1_oja.value))
+                dw2_oja = gamma*(self.weight1_oja.value*q1+self.weight2_oja.value*q2)*(q2-((self.weight1_oja.value*q1+self.weight2_oja.value*q2)*self.weight2_oja.value))
+                self.weight1_oja.value = self.weight1_oja.value+(dw1_oja*dt)
+                self.weight2_oja.value = self.weight2_oja.value+(dw2_oja*dt)
 
-            #################### Gradient Descent ###################
-            alpha = 0.2
-            q = np.array([q1,q2])
-            weights_gd = np.array([self.weight1_gd.value, self.weight2_gd.value])
-            # estimated joint angles
-            q_est = weights_gd.dot(q)*weights_gd
-            # error function
-            L = 1/2*(q_est-q)**2
-            # gradient descent
-            L_dw1 = q[0]*self.weight2_gd.value*(q_est[0]-q[0])
-            L_dw2 = q[1]*self.weight1_gd.value*(q_est[1]-q[1])
+                #################### Gradient Descent ###################
+                alpha = 0.2
+                q = np.array([q1,q2])
+                weights_gd = np.array([self.weight1_gd.value, self.weight2_gd.value])
+                # estimated joint angles
+                q_est = weights_gd.dot(q)*weights_gd
+                # error function
+                L = 1/2*(q_est-q)**2
+                # gradient descent
+                L_dw1 = q[0]*self.weight2_gd.value*(q_est[0]-q[0])
+                L_dw2 = q[1]*self.weight1_gd.value*(q_est[1]-q[1])
 
-            self.weight1_gd.value = self.weight1_gd.value - alpha*L_dw1
-            self.weight2_gd.value = self.weight2_gd.value - alpha*L_dw2
-
+                self.weight1_gd.value = self.weight1_gd.value - alpha*L_dw1
+                self.weight2_gd.value = self.weight2_gd.value - alpha*L_dw2
 
     #def shutdown(self):
     #    pass
